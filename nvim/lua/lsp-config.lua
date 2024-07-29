791894bee7e4cd17906b1a9beca901a99811514d
xKrusty <xxjokerxx500@gmail.com>
2024-07-29 17:27:37 +0200

    updated nvim lsp

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -9,6 +9,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)
 end)
 
 -- automatic installs
