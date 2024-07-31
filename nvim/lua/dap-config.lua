4607cc0d581b4947b03f54ad2d97118f8ee441b1
xKrusty <xxjokerxx500@gmail.com>
2024-07-31 23:31:20 +0200

    updated dap + WT disabled some ligatures

--- nvim/lua/dap-config.lua
+++ nvim/lua/dap-config.lua
@@ -156,6 +156,7 @@ vim.keymap.set("n", "<leader>i", function() dapui.eval(nil, { enter = true }) en
 
 vim.keymap.set("n", "<F5>", dap.continue)
 vim.keymap.set("n", "<F6>", dap.pause)
+vim.keymap.set("n", "<F7>", dap.restart)
 vim.keymap.set("n", "<F10>",dap.step_over)
 vim.keymap.set("n", "<F11>", dap.step_into)
 vim.keymap.set("n", "<F12>", dap.step_out)
