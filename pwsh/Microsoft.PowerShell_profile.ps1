ada068b15bcf8c8b61d50da8dd64b81328ffe058
xKrusty <xxjokerxx500@gmail.com>
2024-08-01 03:19:24 +0200

    updated pwsh added better cd command

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -25,11 +25,43 @@ function reload # simple reload config command
     . $PROFILE
 }
 
-# custom Prompt
-function prompt
+function prompt # custom prompt
 {
     $ESC = [char]27
     "$ESC[38;2;115;140;190m`u{e70f} $env:USERNAME ~ $($executionContext.SessionState.Path.CurrentLocation)`r`n$ESC[38;2;180;210;230m⮞ "
 
     # PSReadLine highlighting can change the prompt color :(
+}
+
+function match-drive([string]$arg)
+{
+    # match Drive letter
+    if (($arg.Length -eq 1) -and ($arg -match "[a-zA-Z]") )
+    {
+        $arg = $arg + ":\"
+    }
+    return $arg
+}
+
+function d([string]$arg) # find dir through fzf
+{
+    $arg = match-drive($arg)
+    fd --type d . $arg | fzf -e | cd
+}
+
+function dd([string]$drive, [string] $dir) # find dir without fzf
+{
+    $drive = match-drive($drive)
+    $results_str = fd --type d . $drive
+    $results = $results_str.Split("\n")
+    <#
+    $results | ForEach-Object {
+        if ($_.Contains($dir, "InvariantCultureIgnoreCase"))
+        {
+            cd $_
+            return
+        }
+    }
+    #>
+    $results | where {$_.Contains($dir, "InvariantCultureIgnoreCase")} | select -First 1 | cd
 }
