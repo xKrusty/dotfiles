02e3130fd749183c847898df19bcaa8f250ced57
xKrusty <xxjokerxx500@gmail.com>
2024-11-07 16:18:30 +0100

    lsp added telescope reference search

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -6,7 +6,14 @@ lsp.on_attach(function(client, bufnr)
 
     local opts= {buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() 
+                                            local success, telescope = pcall(require, "telescope.builtin")
+                                            if success then
+                                                telescope.lsp_references()
+                                            else
+                                                vim.lsp.buf.references() 
+                                            end
+                                        end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)
